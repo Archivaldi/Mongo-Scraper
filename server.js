45d03b06ded96afc63a9757f058cfaa28101c853
Artur Markov <artur.markov1860@gmail.com>
2019-10-23 08:12:25 -0700

    files added

---
+++ server.js
@@ -0,0 +1,42 @@
+var express = require("express");
+var app = express();
+
+app.set("view engine", "ejs");
+
+var bodyParser = require("body-parser");
+
+var cheerio = require("cheerio");
+
+var mongojs = require("mongojs");
+
+var databaseUrl = "scraper";
+var collections = ["scrapedData"];
+
+var request = require("request");
+
+var db = mongojs(databaseUrl, collections);
+db.on("error", function(error) {
+  console.log("Database Error:", error);
+});
+
+
+
+// Main route (simple Hello World Message)
+app.get("/", function(req, res) {
+    res.render("index");
+  });
+
+
+  request.get("https://www.foxnews.com/politics", function(err,response, body){
+      var $ = cheerio.load(body);
+      var results = [];
+      $("h4.title").each(function(i, element){
+        var title = $(element).text();
+        results.push({Title: title});
+      })
+      console.log(results);
+  })
+
+  app.listen("3000", function(){
+      console.log("Server running on 3000");
+  });
